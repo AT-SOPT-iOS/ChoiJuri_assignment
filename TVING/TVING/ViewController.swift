503a10098455be96376fa41d607efe1927539378
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-21 15:50:40 +0900

    initial commit

---
+++ TVING/TVING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVING
+//
+//  Created by 최주리 on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
