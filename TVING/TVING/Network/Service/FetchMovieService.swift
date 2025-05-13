d68a7a57e4e580821136f4e6c9874a26aee48705
JURI <80569323+juri123123@users.noreply.github.com>
2025-05-13 17:21:05 +0900

    feat: mock 객체 생성

--- TVING/TVING/Network/Service/FetchMovieService.swift
+++ TVING/TVING/Network/Service/FetchMovieService.swift
@@ -7,12 +7,30 @@
 
 import Foundation
 
-final class FetchMovieService {
+protocol FetchMovie {
+    func fetchMovie(date: String) async throws -> MovieChartResponse
+}
+
+final class DefaultFetchMovieService: FetchMovie {
     private let network = BaseService.shared
     
     func fetchMovie(date: String) async throws -> MovieChartResponse {
         let url = Endpoint.fetchMovie(date: date).url
         
         return try await network.getRequest(url: url)
+    }
+}
+
+final class MockFetchMovieService: FetchMovie {
+    func fetchMovie(date: String) async throws -> MovieChartResponse {
+        return .init(
+            boxOfficeResult: .init(
+                boxofficeType: "",
+                showRange: "",
+                dailyBoxOfficeList: [
+                    .init(rank: "1", movieNm: "영화 이름1", openDt: "2024-11-11"),
+                ]
+            )
+        )
     }
 }
